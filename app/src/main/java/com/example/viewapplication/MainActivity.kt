e57f3cf79b91d808ac409c71704e4f5348c09ba1
freemake-061 <iizuka@freemake.co.jp>
2024-08-07 15:25:31 +0900

    "こんにちは"表示

--- app/src/main/java/com/example/viewapplication/MainActivity.kt
+++ app/src/main/java/com/example/viewapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.viewapplication
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = "こんにちは"
     }
 }
